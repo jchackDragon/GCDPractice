21cde6059267d58adf33f4c8a78d609b762786d0
jchackDragon <juancarloslopez@Juans-MacBook-Pro.local>
2017-02-01 07:54:58 -0400

    Adding global queue to withBigImage compleation handler

--- GCDPractice/ViewController.swift
+++ GCDPractice/ViewController.swift
@@ -29,7 +29,9 @@ class ViewController: UIViewController {
     }
     
     
-    //This method will going to stop the Main thread
+    // This method downloads a huge image, blocking the main queue and
+    // the UI.
+    // This si for instructional purposes only, never do this.
     @IBAction func synchronousDownload(_ sender: UIBarButtonItem) {
         
         self.photoImage.image = nil
@@ -47,7 +49,8 @@ class ViewController: UIViewController {
     }
     
     
-    //Creating async queue to run proces in background
+    // This method avoids blocking by creating a new queue that runs
+    // in the background, without blocking the UI.
     @IBAction func asynchronousDownload(_ sender: UIBarButtonItem){
         
        self.photoImage.image = nil
@@ -72,7 +75,8 @@ class ViewController: UIViewController {
         
     }
     
-    //Using asycn queue and ecaping closure to run process in background
+    // This code downloads the huge image in a global queue and uses a completion
+    // closure.
     @IBAction func asynchronousDownloadWithScapinClosure(_ sender: UIBarButtonItem){
         
         withBigImage { (image) in
@@ -81,11 +85,15 @@ class ViewController: UIViewController {
         }
     }
     
+    // This method downloads and image in the background once it's
+    // finished, it runs the closure it receives as a parameter.
+    // This closure is called a completion handler
+    // Go download the image, and once you're done, do _this_ (the completion handler)
     func withBigImage(complaitionHandler handler: @escaping (_ image: UIImage) -> Void){
         
         self.activityIndicator.startAnimating()
         
-        DispatchQueue(label:"dowloadQueue").async{
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive).async{
             
             if let url = URL(string:images.vote.rawValue),
                 let data = try? Data(contentsOf:url){
